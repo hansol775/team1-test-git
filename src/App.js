e18f4897e28b85cc16a56ea1b2f990e246745590
2Nagyeom <artest1081@gmail.com>
2024-07-12 16:29:22 +0900

    add : useState Number add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, {useState} from 'react';
 
 const App = () => {
-  const [state, useState] = ('');
+  const [state, useState] = useState('')
+  const [number, setNumber] = useState(0)
 
   return (
     <div>
       <h1>Hello World!</h1>
-      <h1>안녕 나는 이나</h1>
-      <h1>안녕 나는 이나</h1>
-      <h1>안녕 나는 이나</h1>
-      <h1>안녕 나는 이나</h1>
-      <h1>안녕 나는 이나</h1>
-      <h1>안녕 나는 이나</h1>
+      <button onClick={()=> setNumber(number + 1)}>+</button>
+      <h1>{number}</h1>
     </div>
   )
 }
