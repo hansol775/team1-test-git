4e08e14e5a86bec9a10d000f57ffd3d16b34588d
hansol775 <axscdv0@naver.com>
2024-07-12 16:26:46 +0900

    add: state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, {useState} from 'react';
+
 const App = () => {
+  const [state, useState] = ('');
+
   return (
     <div>
       <h1>Hello World!</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
       <h1>안녕 나는 이나</h1>
     </div>
   )
