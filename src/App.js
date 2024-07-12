10e5f64613e7a1ae75a863da2733335600fd3152
ChangHyeon Lee <canghaun@gmail.com>
2024-07-12 16:37:44 +0900

    fix: merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React, {useState} from 'react';
+
 const App = () => {
+  const [state, useState] = ('');
+
   return (
     <div>
       <h1>Hello World!</h1>
-      <h1>안녕 나는 이나겸</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
+      <h1>안녕 나는 이나</h1>
     </div>
   )
 }
